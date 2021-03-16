b2cbef79664fb4a09db1c7001080b6eade7fbad1
Guillaume Bonnet <stabla@protonmail.com>
2021-03-16 11:31:09 +0100

    Update hola.py

--- hola.py
+++ hola.py
@@ -4,3 +4,11 @@ def hello():
     token = "S5mJ4gBkD9oh"
     token = "thisisapassword"
     secret = "dkoqkodqz"
+    database.name = "marie"
+    db.password = "DlZEoskwnKSozlsMpzl"
+    pwd = "jesus"
+    api_key = '12c6724d-0066-43c0-b5a6-8971bee69edc'
+    google-api-key = {
+       key:"AlzaSy2321142cad924df0cb3abf693d464f4e5"  
+       other:"sk_live_EeHnL644i6zo4lyq4v1KdV9H"
+    }
