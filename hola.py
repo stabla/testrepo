199a1a7144afd862d7940f506d59eff6d36aedc6
Guillaume Bonnet <stabla@protonmail.com>
2021-03-16 10:48:20 +0100

    Update hola.py

--- hola.py
+++ hola.py
@@ -1,5 +1,6 @@
 def hello():
     print("what to say")
-    a = "dzkqodzk"
-    token = "ojijdqiz"
+    a = "S5mJ4gBkD9oh"
+    token = "S5mJ4gBkD9oh"
+    token = "thisisapassword"
     secret = "dkoqkodqz"
