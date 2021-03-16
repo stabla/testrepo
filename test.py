5249c3a622a6f7a0fb91d90a9b8c6c7b6d470223
Guillaume Bonnet <stabla@protonmail.com>
2021-03-16 11:34:04 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,19 @@
+import os
+token = "S5mJ4gBkD9oh"
+
+google-api-key = {
+   key:"AlzaSy2321142cad924df0cb3abf693d464f4e5"  
+   other:"sk_live_EeHnL644i6zo4lyq4v1KdV9H"
+}
+
+token = "thisisapassword"
+
+
 def main():
     lol = "dkooqkd"
     nothing= "yep"
      password="dkelepMain"
     return False
     
+
+   
